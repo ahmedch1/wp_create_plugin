7dd0bafc8398353b7f3f0def481d694d855d45b3
Ahmed Ch <ahmedchouihi@gmail.com>
2022-06-12 19:16:17 +0100

    addMEnuForPluginLesson9

--- wp-content/plugins/ahmed-plugin/ahmed-plugin.php
+++ wp-content/plugins/ahmed-plugin/ahmed-plugin.php
@@ -15,8 +15,30 @@ defined('ABSPATH') or die('Hey you can\'t acces this file !');
 
 if(!class_exists('AhmedPlugin')) {
 	class AhmedPlugin {
+		public $plugin;
+		function __construct(){
+			$this->plugin=plugin_basename(__FILE__);
+		}
 		function register() {
 			add_action('admin_enqueue_scripts',array($this,'enqueue'));
+
+			add_action('admin_menu',array($this,'add_admin_pages'));
+
+			add_filter("plugin_action_links_$this->plugin",array($this,'settings_link'));
+		}
+		public function settings_link($links){
+			$settings_link='<a href="admin.php?page=ahmed_plugin">Settings</a>';
+			array_push($links,$settings_link);
+			return $links;
+		}
+		public function add_admin_pages(){
+			add_menu_page('Ahmed Plugin','Ahmed',
+				'manage_options', 'ahmed_plugin',
+				array($this,'admin_index'),'dashicons-store',110);
+		}
+		public function admin_index(){
+			require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
+			//require template
 		}
 
 		protected function create_post_type() {
@@ -44,7 +66,7 @@ if(!class_exists('AhmedPlugin')) {
 //	AhmedPlugin::register();
 
 //activation
-//require_once plugin_dir_path(__FILE__) . 'inc/ahmed-plugin-activate.php';
+	require_once plugin_dir_path(__FILE__) . 'inc/ahmed-plugin-activate.php';
 	register_activation_hook( __FILE__, array( $ahmedPlugin, 'activate' ) );
 
 //deactivation
