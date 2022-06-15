1e635afe1c3296fb1dcbe12ae6a6394313183e01
Ahmed Ch <ahmedchouihi@gmail.com>
2022-06-16 00:13:51 +0100

    starterPluginStructure

--- wp-content/plugins/ahmed-plugin/ahmed-plugin.php
+++ wp-content/plugins/ahmed-plugin/ahmed-plugin.php
@@ -11,6 +11,9 @@
  * Author URI: ahmedchouihi.com
  */
 
+use Inc\Base\Activate;
+use Inc\Base\Deactivate;
+
 defined('ABSPATH') or die('Hey you can\'t acces this file !');
 
 if(file_exists(dirname(__FILE__).'/vendor/autoload.php')){
@@ -19,6 +22,30 @@ if(file_exists(dirname(__FILE__).'/vendor/autoload.php')){
 
 define('PLUGIN_PATH',plugin_dir_path(__FILE__));
 define('PLUGIN_URL',plugin_dir_url(__FILE__));
+define('PLUGIN',plugin_basename(__FILE__));
+
+
+/**
+ * The code that runs during plugin activation
+ */
+function activate_ahmed_plugin(){
+	Activate::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation
+ */
+function deactivate_ahmed_plugin(){
+	Deactivate::deactivate();
+}
+
+register_deactivation_hook(__FILE__,'activate_ahmed_plugin');
+register_deactivation_hook(__FILE__,'deactivate_ahmed_plugin');
+
+
+/**
+ * Initialize all the core classes of the plugin
+ */
 if(class_exists('Inc\\Init')){
 	Inc\Init::register_services();
 }
